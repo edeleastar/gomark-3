22078d3932607049157e4138e7621b4e930ac114
Eamonn de Leastar <edeleastar@wit.ie>
2017-02-10 12:52:46 +0000

    pass properties from controller to view

--- controllers/start.js
+++ controllers/start.js
@@ -6,7 +6,8 @@ const start = {
   index(request, response) {
     logger.info('start rendering');
     const viewData = {
-      title: 'Welcome to Template 1',
+      title: 'Welcome to Gomark 1',
+      description: 'This app enable you to manage your bookmarks. You can organise them into categories and share them with your friends and colleagues.',
     };
     response.render('start', viewData);
   },
