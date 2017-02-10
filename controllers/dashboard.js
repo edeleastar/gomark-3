24aeea260a0fe090b1666065779c082e0525db14
Eamonn de Leastar <edeleastar@wit.ie>
2017-02-10 14:31:52 +0000

    import this module into dashboard controller, and pass to the dashboard view

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const bookmarklistCollection = require('../models/bookmarklist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
       title: 'Template 1 Dashboard',
+      bookmarklistCollection: bookmarklistCollection,
     };
     response.render('dashboard', viewData);
   },
