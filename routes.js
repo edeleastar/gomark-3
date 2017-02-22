46b7adb3c0a9097b3d334fb583b2756d240de867
Eamonn de Leastar <edeleastar@wit.ie>
2017-02-22 16:41:07 +0000

    new routes for bookmarklist display and delete bookmark

--- routes.js
+++ routes.js
@@ -6,9 +6,12 @@ const router = express.Router();
 const start = require('./controllers/start');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
+const bookmarklist = require('./controllers/bookmarklist.js');
 
 router.get('/', start.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
+router.get('/bookmarklist/:id', bookmarklist.index);
+router.get('/bookmarklist/:id/deletebookmark/:bookmarkid', bookmarklist.deleteBookmark);
 
 module.exports = router;
