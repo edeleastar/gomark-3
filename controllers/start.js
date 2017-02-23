3088d4db7efd67b301d72b6d0dd120009476320b
Eamonn de Leastar <edeleastar@wit.ie>
2017-02-23 09:13:15 +0000

    calculate stats and send to view

--- controllers/start.js
+++ controllers/start.js
@@ -1,13 +1,23 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const bookmarklistStore = require('../models/bookmarklist-store.js');
 
 const start = {
   index(request, response) {
     logger.info('start rendering');
+
+    const bookmarkCollections = bookmarklistStore.getBookmarkListCollection();
+    let totalBookmarks = 0;
+    for (let i = 0; i < bookmarkCollections.length; i++) {
+      totalBookmarks = totalBookmarks + bookmarkCollections[i].bookmarks.length;
+    }
+
     const viewData = {
       title: 'Welcome to Gomark 1',
       description: 'This app enable you to manage your bookmarks. You can organise them into categories and share them with your friends and colleagues.',
+      totalNumberOfCollections: bookmarkCollections.length,
+      totalBookmarks: totalBookmarks,
     };
     response.render('start', viewData);
   },
