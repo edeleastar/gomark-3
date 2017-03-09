5174ebebc1c32e1d4a877346f831acdbbbb6442b
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-09 15:34:28 +0000

    message support in about

--- controllers/about.js
+++ controllers/about.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const messageStore = require('../models/message-store');
 
 const about = {
   index(request, response) {
@@ -8,9 +9,20 @@ const about = {
     const viewData = {
       title: 'About Gomark 1',
       contact: 'Developed by GoMark Associates, WIT, Waterford.',
+      messages: messageStore.getAllMessages(),
     };
     response.render('about', viewData);
   },
+
+  addMessage(request, response) {
+    const message = request.body.message;
+    logger.info('message recieved' + message);
+    const newMessage = {
+      messagetext: message,
+    };
+    messageStore.addMessage(newMessage);
+    response.redirect ('/about');
+},
 };
 
 module.exports = about;
