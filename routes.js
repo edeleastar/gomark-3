b5714f6d8d277a44053dc60aaec9a14bcee5ca3b
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-09 15:14:00 +0000

    routes and partials to support add and delete

--- routes.js
+++ routes.js
@@ -10,8 +10,12 @@ const bookmarklist = require('./controllers/bookmarklist.js');
 
 router.get('/', start.index);
 router.get('/dashboard', dashboard.index);
+router.post('/dashboard/addbookmarklist', dashboard.addBookmarklist);
+router.get('/dashboard/deletebookmarklist/:id', dashboard.deleteBookmarklist);
+
 router.get('/about', about.index);
 router.get('/bookmarklist/:id', bookmarklist.index);
 router.get('/bookmarklist/:id/deletebookmark/:bookmarkid', bookmarklist.deleteBookmark);
+router.post('/bookmarklist/:id/addbookmark', bookmarklist.addBookmark);
 
 module.exports = router;
